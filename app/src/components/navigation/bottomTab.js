a4866150e6ab0dadce74332d78cee2c01e697880
luis.amu <113150322+Amu909@users.noreply.github.com>
2025-04-07 19:30:12 -0500

    Bottomtab

--- app/src/components/navigation/bottomTab.js
+++ app/src/components/navigation/bottomTab.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 const BottomTab = () => {
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.tab}>
-        <Text>Inicio</Text>
+        <Text>
+        <MaterialCommunityIcons name="home-plus" size={35} color="#3498db" />
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.tab}>
-        <Text>Helpdesk</Text>
+        <Text>
+        <MaterialIcons name="support-agent" size={35} color="#3498db" />
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -27,7 +33,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderTopWidth: 1,
     borderTopColor: '#ccc',
-    zIndex: 999, // aseguramos que esté encima
+    zIndex: 999,
+    borderRadius :60 // aseguramos que esté encima
   },
   tab: {
     flex: 1,
